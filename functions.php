6bc670507007dc38bf5383a18b6df840d35ea7fa
Matt Beall <mbeall@starverte.com>
2013-01-15 00:16:26 -0700

    Update functions.php
    Add function the_post_thumbnail_caption

--- functions.php
+++ functions.php
@@ -431,3 +431,13 @@ class Tool_Colors_Widget extends WP_Widget {
 	<?php
 	}
 }
+function the_post_thumbnail_caption() {
+  global $post;
+
+  $thumbnail_id    = get_post_thumbnail_id($post->ID);
+  $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
+
+  if ($thumbnail_image && isset($thumbnail_image[0])) {
+    echo '<div class="caption">'.$thumbnail_image[0]->post_excerpt.'</div>';
+  }
+}
