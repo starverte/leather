9b4600460f46e7e9b52860c334d0629742c68f5f
Matt Beall <mbeall@starverte.com>
2013-01-16 17:55:00 -0700

    Update content-sp_event.php
    Add month, day, start time, end time, and location

--- content-sp_event.php
+++ content-sp_event.php
@@ -5,12 +5,27 @@
 ?>
 
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php	$custom = get_post_custom();
+			$event_loc = $custom['event_loc'][0];
+			$event_start = $custom['event_start'][0];
+			$event_end = $custom['event_end'][0];
+			$start_mth = date( 'M', $event_start );
+			$start_day = date( 'j', $event_start );
+			$start_time = date( 'g:i a', $event_start );
+			$end_time = date( 'g:i a', $event_end ); ?>
+			
                     <header class="entry-header">
+                    	<div class="event-date">
+                    		<span class="month"><?php echo $start_mth; ?></span>
+                    		<span class="day"><?php echo $start_day; ?></span>
+                    	</div>
                         <h1 class="entry-title"><?php the_title(); ?></h1>
+                        <p class="event-time"><?php echo $start_time; ?> to <?php echo $end_time; ?>
                     </header><!-- .entry-header -->
                     
                     <div class="entry-content">
                         <?php the_content(); ?>
+                        <a class="directions" href="https://maps.google.com/maps?q=<?php echo $event_loc; ?>">Directions</a>
                     </div><!-- .entry-content -->
 		    <footer id="entry-meta">
                         <?php edit_post_link( __( 'Edit', 'leather' ), '<span class="edit-link">', '</span>' ); ?>
