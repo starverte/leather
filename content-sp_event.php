f4a10c11708b44075687e3cde5c5353f300ade87
Matt Beall <mbeall@starverte.com>
2013-01-16 18:57:12 -0700

    Update content-sp_event.php

--- content-sp_event.php
+++ content-sp_event.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> class="grid_10"">
                 <?php	$custom = get_post_custom();
 			$event_loc = $custom['event_loc'][0];
 			$event_start = $custom['event_start'][0];
@@ -14,20 +14,22 @@
 			$start_time = date( 'g:i a', $event_start );
 			$end_time = date( 'g:i a', $event_end ); ?>
 			
-                    <header class="entry-header">
-                    	<div class="event-date">
+			<div class="event-date grid_1 alpha">
                     		<div class="month"><?php echo $start_mth; ?></div>
                     		<div class="day"><?php echo $start_day; ?></div>
                     	</div>
-                        <h1 class="entry-title"><?php the_title(); ?></h1>
-                        <p class="event-time"><?php echo $start_time; ?> to <?php echo $end_time; ?>
-                    </header><!-- .entry-header -->
+			
+                    	<header class="entry-header grid_8 omega">
+                        	<h1 class="entry-title"><?php the_title(); ?></h1>
+                        	<p class="event-time"><?php echo $start_time; ?> to <?php echo $end_time; ?></p>
+                    	</header><!-- .entry-header -->
                     
-                    <div class="entry-content">
-                        <?php the_content(); ?>
-                        <a class="directions" href="https://maps.google.com/maps?q=<?php echo $event_loc; ?>">Directions</a>
-                    </div><!-- .entry-content -->
-		    <footer id="entry-meta">
-                        <?php edit_post_link( __( 'Edit', 'leather' ), '<span class="edit-link">', '</span>' ); ?>
-                    </footer><!-- #entry-meta -->
+                    	<div class="entry-content grid_8 push_1 alpha">
+                        	<?php the_content(); ?>
+                        	<a class="directions" href="https://maps.google.com/maps?q=<?php echo $event_loc; ?>">Directions</a>
+                    	</div><!-- .entry-content -->
+                    	
+		   	 <footer id="entry-meta">
+                       		<?php edit_post_link( __( 'Edit', 'leather' ), '<span class="edit-link">', '</span>' ); ?>
+                    	</footer><!-- #entry-meta -->
                 </article><!-- #post-<?php the_ID(); ?> -->
