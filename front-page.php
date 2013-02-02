43967fb95014be27fc31ae728f2cc4dbf261dec3
Matt Beall <mbeall@starverte.com>
2013-02-02 14:45:32 -0700

    Update front-page.php
    Remove broken image

--- front-page.php
+++ front-page.php
@@ -12,8 +12,7 @@ get_header(); ?>
                 <section id="primary" class="grid_10 alpha">
 
 		    <div class="royalSlider rsDefault">
-    			<img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/051.jpg" alt="cover" />
-			<img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/65.jpg" />
+    			<img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/65.jpg" alt="cover" />
     			<img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/145.jpg" />
                         <img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/145cf.jpg" />
                         <img class="rsImg" src="http://fortcollinscreative.com/lewnme/wp-content/uploads/2013/01/SacTote.jpg" />
